5538fedffb9b118c0c9717a9eaa3989ce20fb230
d10gomonteiro <diomoonteiro@gmail.com>
2021-10-23 13:58:41 -0300

    Adicionada a lógica de alguns métodos da classe Moto

--- Atividade_05/Lapiseira/solver.ts
+++ Atividade_05/Lapiseira/solver.ts
@@ -1,42 +1,70 @@
 
 class Pessoa {
-    public idade: number;
-    public nome: string;
-
+    idade: number;
+    nome: string;
     constructor (idade: number, nome: string) {
         this.idade = idade;
         this.nome = nome;
     }
-    
-
     toString(): string {
         return "Pessoa: " + this.idade + ":" + this.nome;
     }
 }
+//let pessoa = new Pessoa(10, "kkk");
+//console.log(""+pessoa);
 
 //agregação
 class Moto {
-    public p1: Pessoa;
-    public power: number;
-    public time: number;
+    pessoa: Pessoa | null;
+    power: number;
+    time: number;
 
-    constructor(p1: Pessoa, power: number, time: number) {
-        this.p1 = p1;
-        this.power = power;
-        this.time = time;
+    constructor(power: number) {
+        this.pessoa = null;
+        this.power = 1;
+        this.time = 0;
     }
-
-    public Motocycle(power: number){ //Comprar mais tempo
-      
+    public buy(time:number){
+        this.time += 5;
     }
+    public gasto(){
+        this.time-=1;
+    }
+    public drive(time:number): boolean{
+        if(this.pessoa==null)
+            console.log("não tem ninguem para dirigir");
+            return false;
 
-    public buy(time:number){ //Se estiver vazio, coloca a pessoa na moto e retorna true
+        let gast = this.moto.gasto()*metros;
+        if(gast <= this.moto.time){
+            this.moto.time -= gast;
+        }
         
     }
 
-    public in(p1: Pessoa): boolean {
-  //Se houver uma person, retira e retorna
-    //Se não, retorna null
-}
+    public in(pessoa: Pessoa): boolean {
+        if(this.pessoa != null){
+            console.log("tem gente");
+            return false;
+        }
+        this.pessoa = pessoa;
+        return true;
+    }
 
+    public out():Pessoa | null{
+        if(this.pessoa == null){
+            console.log("não tem ninguem");
+            return null;
+        }
+        let pessoa = this.pessoa;
+        this.pessoa = null;
+        return pessoa;
+    }
+    public honk(){
+
+    }
+
+    toString(): string {
+        return "Pessoa: " + this.idade + ":" + this.nome;
+    }
 }
